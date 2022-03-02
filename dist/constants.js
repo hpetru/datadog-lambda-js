f52c35b0f96755765ca7787292b28ed1369389fc
Petru Hincu <hincupetru@gmail.com>
2022-03-02 13:09:15 +0200

    test new changes

---
+++ dist/constants.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.datadogLambdaVersion = void 0;
+exports.datadogLambdaVersion = "5.71.0";
+//# sourceMappingURL=constants.js.map
